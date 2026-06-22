30d82cf45fc5285aa7f548f879e89d3ccfc83ea3
Yago Souza <yagosouza.1802@gmail.com>
2026-06-22 02:43:31 -0300

    feat: adiciona metodo salvar no LivroRepository

--- src/main/java/com/ysouz/sistemabiblioteca/repository/LivroRepository.java
+++ src/main/java/com/ysouz/sistemabiblioteca/repository/LivroRepository.java
@@ -1,4 +1,30 @@
 package com.ysouz.sistemabiblioteca.repository;
 
+import com.ysouz.sistemabiblioteca.model.Livro;
+import com.ysouz.sistemabiblioteca.connection.Conexao;
+import com.ysouz.sistemabiblioteca.exception.DatabaseException;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 public class LivroRepository {
+
+    public void salvar(Livro livro) {
+        String query = "INSERT INTO livros VALUES (default , ?, ?, ?, ?, ?)";
+
+        try (Connection conexao = Conexao.getConexao();
+             PreparedStatement statement = conexao.prepareStatement(query)) {
+
+            statement.setString(1, livro.getTitulo());
+            statement.setString(2, livro.getAutor());
+            statement.setString(3, livro.getIsbn());
+            statement.setInt(4, livro.getAnoLancamento());
+            statement.setBoolean(5, livro.isDisponivel());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Erro ao salvar livro.", e);
+        }
+    }
 }
