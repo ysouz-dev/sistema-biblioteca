30929d037e75c03ec12bfcef1f154984661d1922
Yago Souza <yagosouza.1802@gmail.com>
2026-06-22 03:24:17 -0300

    feat: adiciona metodo cadastrarLivro no LivroService

--- src/main/java/com/ysouz/sistemabiblioteca/service/LivroService.java
+++ src/main/java/com/ysouz/sistemabiblioteca/service/LivroService.java
@@ -1,4 +1,20 @@
 package com.ysouz.sistemabiblioteca.service;
 
+import com.ysouz.sistemabiblioteca.model.Livro;
+import com.ysouz.sistemabiblioteca.repository.LivroRepository;
+import com.ysouz.sistemabiblioteca.exception.LivroJaCadastradoException;
+
 public class LivroService {
+    private LivroRepository livroRepository;
+
+    public LivroService() {
+        this.livroRepository = new LivroRepository();
+    }
+
+    public void cadastrarLivro(Livro livro) {
+        if (this.livroRepository.containsLivro(livro.getIsbn())) {
+            throw new LivroJaCadastradoException("Livro já cadastrado no sistema.");
+        }
+        this.livroRepository.salvar(livro);
+    }
 }
