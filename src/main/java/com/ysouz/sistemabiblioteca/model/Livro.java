e3bd69fdae0559db0f84fd1bea97badd83c0275d
Yago Souza <yagosouza.1802@gmail.com>
2026-06-22 01:06:25 -0300

    feat: adiciona atributos e construtor de Livro

--- src/main/java/com/ysouz/sistemabiblioteca/model/Livro.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Livro.java
@@ -1,4 +1,16 @@
 package com.ysouz.sistemabiblioteca.model;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int anoLancamento;
+    private boolean disponivel;
+
+    public Livro (String titulo, String autor, String isbn, int anoLancamento) {
+        this.titulo = titulo.strip().toUpperCase();
+        this.autor = autor.strip().toUpperCase();
+        this.isbn = isbn.strip();
+        this.anoLancamento = anoLancamento;
+    }
 }
