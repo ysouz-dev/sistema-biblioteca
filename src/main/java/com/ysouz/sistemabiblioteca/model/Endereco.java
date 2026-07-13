564f276327c487ea703e7646e9aea67039b8b651
Yago Souza <yagosouza.1802@gmail.com>
2026-07-13 20:28:04 -0300

    feat: adiciona atributo cep a classe Endereco

--- src/main/java/com/ysouz/sistemabiblioteca/model/Endereco.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Endereco.java
@@ -4,10 +4,12 @@ public class Endereco {
     private String rua;
     private String bairro;
     private Integer numero;
+    private String cep;
 
-    public Endereco(String rua, Integer numero, String bairro) {
+    public Endereco(String rua, Integer numero, String bairro, String cep) {
         this.rua = rua.strip().toUpperCase();
         this.bairro = bairro.strip().toUpperCase();
         this.numero = numero;
+        this.cep = cep.strip();
     }
 }
