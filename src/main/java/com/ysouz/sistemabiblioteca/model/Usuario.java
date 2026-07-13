2e849b3e0740f2a6170c15dc29196c712994713b
Yago Souza <yagosouza.1802@gmail.com>
2026-07-13 19:06:49 -0300

    feat: adiciona atributos a classe Usuario

--- src/main/java/com/ysouz/sistemabiblioteca/model/Usuario.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Usuario.java
@@ -1,4 +1,13 @@
 package com.ysouz.sistemabiblioteca.model;
 
 public class Usuario {
+    private String nome;
+    private String cpf;
+    private Endereco endereco;
+
+    public Usuario(String nome, String cpf, Endereco endereco) {
+        this.nome = nome.strip().toUpperCase();
+        this.cpf = cpf.strip();
+        this.endereco = endereco;
+    }
 }
