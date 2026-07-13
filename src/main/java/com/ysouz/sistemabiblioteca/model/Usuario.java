a44ab9336208eae3f1396bbae7dc1b97f155d399
Yago Souza <yagosouza.1802@gmail.com>
2026-07-13 19:23:56 -0300

    feat: implementa validacoes no construtor de Usuario

--- src/main/java/com/ysouz/sistemabiblioteca/model/Usuario.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Usuario.java
@@ -1,11 +1,17 @@
 package com.ysouz.sistemabiblioteca.model;
 
+import com.ysouz.sistemabiblioteca.validation.UsuarioValidator;
+
 public class Usuario {
     private String nome;
     private String cpf;
     private Endereco endereco;
 
     public Usuario(String nome, String cpf, Endereco endereco) {
+        UsuarioValidator.validaNome(nome);
+        UsuarioValidator.validaCpf(cpf);
+        UsuarioValidator.validaEndereco(endereco);
+
         this.nome = nome.strip().toUpperCase();
         this.cpf = cpf.strip();
         this.endereco = endereco;
