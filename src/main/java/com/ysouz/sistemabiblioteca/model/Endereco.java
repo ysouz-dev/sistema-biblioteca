2be594b3bc7dce48e98ca9d0a150a0a2aa8bec92
Yago Souza <yagosouza.1802@gmail.com>
2026-07-13 20:40:19 -0300

    refactor: altera o atributo numero para String

--- src/main/java/com/ysouz/sistemabiblioteca/model/Endereco.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Endereco.java
@@ -3,10 +3,10 @@ package com.ysouz.sistemabiblioteca.model;
 public class Endereco {
     private String rua;
     private String bairro;
-    private Integer numero;
+    private String numero;
     private String cep;
 
-    public Endereco(String rua, Integer numero, String bairro, String cep) {
+    public Endereco(String rua, String numero, String bairro, String cep) {
         this.rua = rua.strip().toUpperCase();
         this.bairro = bairro.strip().toUpperCase();
         this.numero = numero;
