5e54b1b166bc6c3a3901287af38dc646d8a9cf78
Yago Souza <yagosouza.1802@gmail.com>
2026-07-13 19:21:18 -0300

    feat: adiciona classe UsuarioValidator

---
+++ src/main/java/com/ysouz/sistemabiblioteca/validation/UsuarioValidator.java
@@ -0,0 +1,39 @@
+package com.ysouz.sistemabiblioteca.validation;
+
+import com.ysouz.sistemabiblioteca.model.Endereco;
+
+import java.util.Objects;
+
+public final class UsuarioValidator {
+
+    public static void validaNome(String nome) {
+         if (Objects.isNull(nome) || nome.strip().length() < 3 || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome inválido.");
+        }
+
+         String nomeSemEspaco = nome.replace(" ", "");
+         for (int i = 0; i < nomeSemEspaco.length(); i++) {
+             if (!Character.isLetter(nomeSemEspaco.charAt(i))) {
+                 throw new IllegalArgumentException("Nome inválido.");
+             }
+         }
+    }
+
+    public static void validaCpf(String cpf) {
+        if (Objects.isNull(cpf) || cpf.strip().length() != 11 || cpf.isBlank()) {
+            throw new IllegalArgumentException("Cpf inválido.");
+        }
+
+        for (int i = 0; i < cpf.strip().length(); i++) {
+            if (!Character.isDigit(cpf.strip().charAt(i))) {
+                throw new IllegalArgumentException("Cpf invalido.");
+            }
+        }
+    }
+
+    public static void validaEndereco(Endereco endereco) {
+        if (Objects.isNull(endereco)) {
+            throw new IllegalArgumentException("Endereço inválido.");
+        }
+    }
+}
