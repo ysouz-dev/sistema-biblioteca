098035a1ab867aadfdda55809a563fe77d6d2169
Yago Souza <yagosouza.1802@gmail.com>
2026-07-14 14:04:17 -0300

    feat: adiciona metodo cadastrarUsuario a UsuarioService

--- src/main/java/com/ysouz/sistemabiblioteca/service/UsuarioService.java
+++ src/main/java/com/ysouz/sistemabiblioteca/service/UsuarioService.java
@@ -1,4 +1,21 @@
 package com.ysouz.sistemabiblioteca.service;
 
+import com.ysouz.sistemabiblioteca.exception.UsuarioJaCadastradoException;
+import com.ysouz.sistemabiblioteca.repository.UsuarioRepository;
+import com.ysouz.sistemabiblioteca.model.Usuario;
+
+
 public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService() {
+        this.usuarioRepository = new UsuarioRepository();
+    }
+
+    public void cadastrarUsuario(Usuario usuario) {
+        if (this.usuarioRepository.containsUsuario(usuario.getCpf())) {
+            throw new UsuarioJaCadastradoException("Usuário já cadastrado no sistema.");
+        }
+        this.usuarioRepository.salvar(usuario);
+    }
 }
