d936b8e63672efbd8727b3d87400225c0d6c88cc
Yago Souza <yagosouza.1802@gmail.com>
2026-07-16 01:16:30 -0300

    feat: adiciona validacao de parametros de Emprestimo

--- src/main/java/com/ysouz/sistemabiblioteca/model/Emprestimo.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Emprestimo.java
@@ -1,6 +1,7 @@
 package com.ysouz.sistemabiblioteca.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Emprestimo {
     private Usuario usuario;
@@ -8,6 +9,10 @@ public class Emprestimo {
     private LocalDate data;
 
     public Emprestimo(Usuario usuario, Livro livro) {
+        if (Objects.isNull(usuario)) {throw new IllegalArgumentException("O usuário não pode ser nulo");}
+        if (Objects.isNull(livro)) {throw new IllegalArgumentException("O livro não pode ser nulo.");}
+        if (!livro.isDisponivel()) {throw new IllegalArgumentException("Um livro indisponivel não pode ser emprestado");}
+
         this.usuario = usuario;
         this.livro = livro;
         this.data = LocalDate.now();
