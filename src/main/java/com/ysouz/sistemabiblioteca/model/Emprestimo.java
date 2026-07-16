9641f6b19488167376026b7a071eaceddbe1cea9
Yago Souza <yagosouza.1802@gmail.com>
2026-07-16 00:59:25 -0300

    feat: adiciona atributos e construtor a classe Emprestimo

--- src/main/java/com/ysouz/sistemabiblioteca/model/Emprestimo.java
+++ src/main/java/com/ysouz/sistemabiblioteca/model/Emprestimo.java
@@ -1,4 +1,15 @@
 package com.ysouz.sistemabiblioteca.model;
 
+import java.time.LocalDate;
+
 public class Emprestimo {
+    private Usuario usuario;
+    private Livro livro;
+    private LocalDate data;
+
+    public Emprestimo(Usuario usuario, Livro livro) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.data = LocalDate.now();
+    }
 }
