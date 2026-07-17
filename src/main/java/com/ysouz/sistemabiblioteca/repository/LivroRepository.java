5e132e2d6cd5fc18b0fbd97a763aa9c8eb47e741
Yago Souza <yagosouza.1802@gmail.com>
2026-07-17 14:47:19 -0300

    feat: adiciona metodo buscaPorIsbn a LivroRepository

--- src/main/java/com/ysouz/sistemabiblioteca/repository/LivroRepository.java
+++ src/main/java/com/ysouz/sistemabiblioteca/repository/LivroRepository.java
@@ -1,5 +1,6 @@
 package com.ysouz.sistemabiblioteca.repository;
 
+import com.ysouz.sistemabiblioteca.exception.LivroNaoEncontradoException;
 import com.ysouz.sistemabiblioteca.model.Livro;
 import com.ysouz.sistemabiblioteca.connection.Conexao;
 import com.ysouz.sistemabiblioteca.exception.DatabaseException;
@@ -41,6 +42,32 @@ public class LivroRepository {
 
         } catch (SQLException e) {
             throw new DatabaseException("Erro ao verificar existência de livro no banco.", e);
+        }
+    }
+
+    public Livro buscaPorIsbn(String isbn) {
+        String query = "SELECT * FROM livros WHERE isbn = ?";
+
+        try (Connection conexao = Conexao.getConexao();
+            PreparedStatement statement = conexao.prepareStatement(query)) {
+
+            statement.setString(1, isbn);
+
+            try (ResultSet rs = statement.executeQuery()) {
+
+                if (rs.next()) {
+                    String titulo = rs.getString("titulo");
+                    String autor = rs.getString("autor");
+                    String ISBN = rs.getString("isbn");
+                    int anoLancamento = rs.getInt("ano_lancamento");
+                    boolean disponivel = rs.getBoolean("disponivel");
+
+                    return new Livro(titulo, autor, ISBN, anoLancamento, disponivel);
+                }
+                throw new LivroNaoEncontradoException("Livro não encontrado no sistema.");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Erro ao buscar Livro no banco", e);
         }
     }
 }
