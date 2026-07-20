b3a4c77cb8cf27c6b9b6c8547bf6f0fe3da0036f
Yago Souza <yagosouza.1802@gmail.com>
2026-07-20 15:56:08 -0300

    feat: adiciona metodo devolverLivro a EmprestimoService

--- src/main/java/com/ysouz/sistemabiblioteca/service/EmprestimoService.java
+++ src/main/java/com/ysouz/sistemabiblioteca/service/EmprestimoService.java
@@ -1,5 +1,6 @@
 package com.ysouz.sistemabiblioteca.service;
 
+import com.ysouz.sistemabiblioteca.exception.EmprestimoNaoEncontradoException;
 import com.ysouz.sistemabiblioteca.model.Emprestimo;
 import com.ysouz.sistemabiblioteca.repository.EmprestimoRepository;
 import com.ysouz.sistemabiblioteca.exception.EmprestimoPendenteException;
@@ -17,5 +18,12 @@ public class EmprestimoService {
             throw new EmprestimoPendenteException("O usuário já possui um empréstimo pendente.");
         }
         this.emprestimoRepository.emprestar(emprestimo);
+    }
+
+    public void devolverLivro(Emprestimo emprestimo) {
+        if (!this.emprestimoRepository.containsEmprestimo(emprestimo)) {
+            throw new EmprestimoNaoEncontradoException("Empréstimo não encontrado no sistema.");
+        }
+        this.emprestimoRepository.devolver(emprestimo);
     }
 }
