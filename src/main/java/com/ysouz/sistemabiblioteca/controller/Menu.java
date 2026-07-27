d16f8517acaf3804cb50f72c48d63c6c0d6e0e18
Yago Souza <yagosouza.1802@gmail.com>
2026-07-26 22:46:41 -0300

    feat: adiciona metodo de exibicao do menu do usuario

--- src/main/java/com/ysouz/sistemabiblioteca/controller/Menu.java
+++ src/main/java/com/ysouz/sistemabiblioteca/controller/Menu.java
@@ -73,6 +73,30 @@ public class Menu {
         return resposta;
     }
 
+    public int menuUsuario() {
+        System.out.println("========= Usuário Opcões ========");
+        System.out.println("[ 1 ] Busca por CPF");
+        System.out.println("[ 2 ] Busca por Nome");
+        System.out.println("[ 0 ] Voltar");
+        System.out.println("-".repeat(25));
+
+        int resposta = Integer.MIN_VALUE;
+        do {
+            try {
+                System.out.print("Digite uma opção: ");
+                resposta = this.scanner.nextInt();
+                this.scanner.nextLine();
+                if (resposta < 0 || resposta > 2) {
+                    System.out.printf("Opção inválida! %d não é uma opção.%n", resposta);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opção inválida! Digite somente números.");
+                this.scanner.nextLine();
+            }
+        } while(resposta < 0 || resposta > 2);
+        return resposta;
+    }
+
     public void cadastrarLivro() {
         this.livroController.cadastrarLivro();
     }
