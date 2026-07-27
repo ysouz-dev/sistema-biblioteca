a8704c8d95ec2e97e4a72fb3bf7eaf222b9a6dd7
Yago Souza <yagosouza.1802@gmail.com>
2026-07-27 00:52:36 -0300

    feat: adiciona opcoes de usuario no metodo menuPrincipal

--- src/main/java/com/ysouz/sistemabiblioteca/controller/Menu.java
+++ src/main/java/com/ysouz/sistemabiblioteca/controller/Menu.java
@@ -26,6 +26,7 @@ public class Menu {
         System.out.println("[ 3 ] Registrar Empréstimo");
         System.out.println("[ 4 ] Devolver Livro");
         System.out.println("[ 5 ] Livro Opcões...");
+        System.out.println("[ 6 ] Usuário Opções...");
         System.out.println("[ 0 ] Encerrar Sistema");
         System.out.println("-".repeat(25));
 
@@ -35,14 +36,14 @@ public class Menu {
                 System.out.print("Digite uma opção: ");
                 resposta = this.scanner.nextInt();
                 this.scanner.nextLine();
-                if (resposta < 0 || resposta > 5) {
+                if (resposta < 0 || resposta > 6) {
                     System.out.printf("Opção inválida! %d não é uma opção.%n", resposta);
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Opção inválida! Digite somente números.");
                 this.scanner.nextLine();
             }
-        } while(resposta < 0 || resposta > 5);
+        } while(resposta < 0 || resposta > 6);
         return resposta;
     }
 
