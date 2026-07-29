8382b6f84006acd4701b86584f9c9e00a6389fdb
Yago Souza <yagosouza.1802@gmail.com>
2026-07-29 20:00:23 -0300

    feat: adiciona metodo menuEmprestimo ao Menu

--- src/main/java/com/ysouz/sistemabiblioteca/controller/Menu.java
+++ src/main/java/com/ysouz/sistemabiblioteca/controller/Menu.java
@@ -100,6 +100,32 @@ public class Menu {
         return resposta;
     }
 
+    public int menuEmprestimo() {
+        System.out.println("========= Empréstimo Opcões ========");
+        System.out.println("[ 1 ] Consultar Empréstimos Pendentes Por CPF");
+        System.out.println("[ 2 ] Consultar Todos Empréstimos realizados Por CPF");
+        System.out.println("[ 3 ] Lista de Todos Empréstimos");
+        System.out.println("[ 4 ] Lista de Todos Empréstimos Pendentes");
+        System.out.println("[ 0 ] Voltar");
+        System.out.println("-".repeat(25));
+
+        int resposta = Integer.MIN_VALUE;
+        do {
+            try {
+                System.out.print("Digite uma opção: ");
+                resposta = this.scanner.nextInt();
+                this.scanner.nextLine();
+                if (resposta < 0 || resposta > 4) {
+                    System.out.printf("Opção inválida! %d não é uma opção.%n", resposta);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Opção inválida! Digite somente números.");
+                this.scanner.nextLine();
+            }
+        } while(resposta < 0 || resposta > 4);
+        return resposta;
+    }
+
     public void cadastrarLivro() {
         this.livroController.cadastrarLivro();
     }
