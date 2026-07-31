421578e586c93844573bdfd176a0eb7d06cba7f9
Yago Souza <yagosouza.1802@gmail.com>
2026-07-31 00:11:48 -0300

    docs: adiciona documentacao de UsuarioJaCadastradoException

--- src/main/java/com/ysouz/sistemabiblioteca/exception/UsuarioJaCadastradoException.java
+++ src/main/java/com/ysouz/sistemabiblioteca/exception/UsuarioJaCadastradoException.java
@@ -1,10 +1,26 @@
 package com.ysouz.sistemabiblioteca.exception;
 
+/**
+ * Lançada quando o sistema tenta cadastrar um usuário com o mesmo
+ * CPF de um usuário já cadastrado anteriormente.
+ */
 public class UsuarioJaCadastradoException extends RuntimeException {
+
+    /**
+     * Cria a exceção com uma messagem de erro.
+     *
+     * @param message descrição do erro
+     */
     public UsuarioJaCadastradoException(String message) {
         super(message);
     }
 
+    /**
+     * Cria a exceção com uma mensagem de erro e a causa original do erro.
+     *
+     * @param message descrição do erro
+     * @param cause exceção original que motivou este erro
+     */
     public UsuarioJaCadastradoException(String message, Throwable cause) {
         super(message, cause);
     }
